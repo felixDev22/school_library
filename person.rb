90681a781f31945a0888139193a50c52e9489677
felixDev22 <felixnzomo@gmail.com>
2023-03-13 19:48:21 +0300

    Create person class

---
+++ person.rb
@@ -0,0 +1,25 @@
+class Person
+  def initialize(name = 'Unknown', age,  parent_permission: true)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @id = Random.rand(1..1000)
+  end
+
+  # getter for id
+  attr_reader :id
+
+  # getter and setter for name and age
+  attr_accessor :name, :age
+
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
