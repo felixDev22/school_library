2438a20599b678321193ec1d1d1fb075d1135214
felixDev22 <felixnzomo@gmail.com>
2023-03-13 19:49:28 +0300

    create student class that inherits from person

---
+++ student.rb
@@ -0,0 +1,12 @@
+require './person'
+
+class Student < Person
+  def initialize(name = 'Unknown', classroom, age, parent_permission: true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
