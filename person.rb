800ef131c946ff0986d40c6bc8f0fd5c86713da1
felixDev22 <felixnzomo@gmail.com>
2023-03-13 20:01:40 +0300

    Update person file, correct typo

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(name = 'Unknown', age,  parent_permission: true)
+  def initialize(name = 'Unknown', age,  parent_permission=true)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -15,11 +15,11 @@ class Person
 
   private
 
-  def of_age?
+   def is_of_age?
     @age >= 18
   end
 
   def can_use_services?
-    of_age? || @parent_permission
+    is_of_age? || @parent_permission
   end
 end
