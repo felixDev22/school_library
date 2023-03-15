1d50e12092a19fabe56ca2fd8b4f323979042045
felixDev22 <felixnzomo@gmail.com>
2023-03-15 16:06:09 +0300

    Update book, add rentals relation

--- book.rb
+++ book.rb
@@ -1,8 +1,13 @@
 class Book
-  attr_accessor :title, :author,
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentails = []
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, self, person)
   end
 end
