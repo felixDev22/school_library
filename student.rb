38140f59a04957bf3ad997b44deb3089c60b130d
felixDev22 <felixnzomo@gmail.com>
2023-03-15 17:02:30 +0300

    Add an unless exception on Students.rb

--- student.rb
+++ student.rb
@@ -14,6 +14,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.add_student(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
