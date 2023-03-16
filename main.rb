3bd1093e5690fef6379df830ef931af329a46300
felixDev22 <felixnzomo@gmail.com>
2023-03-16 23:10:50 +0300

    Update main to nake it short and more readable

--- main.rb
+++ main.rb
@@ -9,11 +9,22 @@ def make_person
   when 2
     create_teacher
   else
-    puts "Invalid input. Please enter either 1 or 2."
+    puts 'Invalid input. Please enter either 1 or 2.'
   end
 end
 
-def list_of_options(user_answer)
+def display_menu
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+def handle_user_input(user_answer)
   case user_answer
   when 1
     all_books
@@ -28,7 +39,7 @@ def list_of_options(user_answer)
   when 6
     user_rentals
   else
-    puts "Invalid input. Please enter a number between 1 and 7."
+    puts 'Invalid input. Please enter a number between 1 and 7.'
   end
 end
 
@@ -36,24 +47,16 @@ def main
   puts 'Welcome to School Library App!'
   loop do
     puts ' '
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-
+    display_menu
     user_answer = gets.chomp.to_i
 
     if user_answer == 7
       puts 'Thank you for using this app'
       break
     elsif !(1..7).include?(user_answer)
-      puts "Invalid input. Please enter a number between 1 and 7."
+      puts 'Invalid input. Please enter a number between 1 and 7.'
       next
     end
-    list_of_options(user_answer)
+    handle_user_input(user_answer)
   end
 end
