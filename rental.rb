d7871b2bb4a72955362acd6b246d972782f4827a
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-21 21:14:02 +0300

    Create hash on rental.rb method to preserve file

--- rental.rb
+++ rental.rb
@@ -9,4 +9,12 @@ class Rental
     person.rentals << self
     book.rentals << self
   end
+
+  def to_h
+    {
+      date: @date,
+      person: @person.to_h,
+      book: @book.to_h
+    }
+  end
 end
