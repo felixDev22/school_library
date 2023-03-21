499e918be0f9c45b87eb80a0965c306f37dfac18
felixDev22 <felixnzomo@gmail.com>
2023-03-21 17:35:32 +0300

    Update preserve data rb file

--- preserve_data.rb
+++ preserve_data.rb
@@ -1,8 +1,10 @@
 require 'JSON'
 
 def save_data(data, filename)
-  save_data_h = data.map(&:to_h) # Convert each object to a hash
-  File.write(filename, JSON.generate(save_data_h))
+  return unless data
+
+  save_input = data.map(&:to_h) # Convert each object to a hash
+  File.write(filename, JSON.generate(save_input))
 end
 
 def load_data(filename)
