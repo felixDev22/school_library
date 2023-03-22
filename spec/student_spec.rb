d87580619d139f9af993bd479d8aa10880d52453
felixDev22 <felixnzomo@gmail.com>
2023-03-22 18:35:25 +0300

    Update student, add test unit

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,2 +1,15 @@
-require_relative '../student.rb'
+require_relative '../student'
 
+describe Student do
+  it 'Should create instance of the student class' do
+    student = Student.new(classroom: 6, age: 13, parent_permission: 'Yes', name: 'Mark')
+    expect(student).to be_an_instance_of Student
+  end
+end
+
+describe '#play_hooky' do
+  it 'should show play_hooky symbol' do
+    student = Student.new(classroom: 6, age: 13, parent_permission: 'Yes', name: 'Mark')
+    expect(student.play_hooky).to eq '¯\(ツ)/¯'
+  end
+end
