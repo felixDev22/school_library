ea590d4db845131b8a5233bf46adbb86734212c4
felixDev22 <felixnzomo@gmail.com>
2023-03-22 17:22:10 +0300

    Add test for teacher, fix typo

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../teacher.rb'
+
+describe 'Teacher instance' do
+  it 'creates an instance of the Teacher class' do
+    teacher = Teacher.new('Chemisty', 35, 'Juma')
+    expect(teacher.class).to eq Teacher
+  end
+end
+
+describe '#specialization' do
+  it 'returns the specialization of the teacher' do
+    teacher = Teacher.new('Chemisty', 35, 'Juma')
+    expect(teacher.specialization).to eq 'Chemisty'
+  end
+end
+
+describe '#can_use_services?' do
+  it 'returns true' do
+    teacher = Teacher.new('Chemisty', 35, 'Juma')
+    expect(teacher.can_use_services?).to eq true
+  end
+end
