72153b6c62b1119dc0e75da8e67d0932a0a88549
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-03-22 19:47:53 +0300

    Write a unit test for rental

---
+++ spec/rental_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../rental'
+require_relative '../person'
+require_relative '../book'
+
+describe Rental do
+  describe '#initialize' do
+    it 'should set the date, book, and person' do
+      book = { 'title' => 'The Great Gatsby', 'author' => 'F. Scott Fitzgerald', 'rentals' => [] }
+      person = { 'name' => 'John Doe', 'age' => 25, 'parent_permission' => true, 'rentals' => [] }
+      date = '2022-01-01'
+      rental = Rental.new(date, person, book)
+
+      expect(rental.date).to eq date
+      expect(rental.book).to eq book
+      expect(rental.person).to eq person
+    end
+
+    it 'should add the rental to the book and person rentals' do
+      book = { 'title' => 'The Great Gatsby', 'author' => 'F. Scott Fitzgerald', 'rentals' => [] }
+      person = { 'name' => 'John Doe', 'age' => 25, 'parent_permission' => true, 'rentals' => [] }
+      date = '2022-01-01'
+      rental = Rental.new(date, person, book)
+
+      expect(book['rentals'].last).to eq rental
+      expect(person['rentals'].last).to eq rental
+    end
+  end
+end
