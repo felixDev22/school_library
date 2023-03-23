411977734f631dc3e9ed3eb27f17b089e0bfe776
felixDev22 <felixnzomo@gmail.com>
2023-03-23 18:02:36 +0300

    Add classroom test on student.rb

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../student'
+require 'spec_helper'
 
 describe Student do
   it 'Should create instance of the student class' do
@@ -11,5 +11,27 @@ describe '#play_hooky' do
   it 'should show play_hooky symbol' do
     student = Student.new(classroom: 6, age: 13, parent_permission: 'Yes', name: 'Mark')
     expect(student.play_hooky).to eq '¯\(ツ)/¯'
+  end
+end
+
+# Classroom test
+describe Student do
+  let(:classroom) { Classroom.new(6) }
+  let(:student) { Student.new(classroom: 5, age: 13, parent_permission: true, name: 'John') }
+
+  describe '#classroom' do
+    context 'when the student is not already in the classroom' do
+      it 'adds the student to the classroom' do
+        expect { student.classroom = classroom }.to change { classroom.students.size }.by(1)
+        expect(classroom.students).to include(student)
+      end
+    end
+
+    context 'when the student is already in the classroom' do
+      it 'does not add the student to the classroom again' do
+        classroom.students.push(student)
+        expect { student.classroom = classroom }.not_to change { classroom.students.size }
+      end
+    end
   end
 end
